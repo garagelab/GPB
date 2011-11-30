6da84ec10c37faaef778235886b02bd9b9dd1fa6
Santiago Coffey <scoffey@itba.edu.ar>
2011-11-29 23:32:19 -0300

    Merging latest items model modifications from master branch

--- gpbscraper/gpbscraper/items.py
+++ gpbscraper/gpbscraper/items.py
@@ -11,25 +11,26 @@ from datetime import date
 
 spanish_date = lambda day,month,year: date(int(year),int(month),int(day))
 
-MONEY_RE = re.compile(r'([\d\.]*),(\d+)')
+MONEY_RE = re.compile(r'^\$?([\d\.]+)(?:,(\d+))?$')
 
 def parse_money(value):
-    pesos, centavos = MONEY_RE.match(value.strip()).groups()
-    pesos = pesos.replace('.', '')
-    return float("%s.%s" % (pesos if pesos != '' else 0, centavos))
+    pesos, centavos = MONEY_RE.match(value).groups()
+    if centavos: return float("%s.%s" % (pesos.replace('.', ''), centavos))
+    else: return float(pesos)
 
 class ProveedorItem(Item):
     nombre = Field(output_processor=TakeFirst())
     domicilio = Field(output_processor=TakeFirst())
     cuit = Field(output_processor=TakeFirst())
     localidad = Field(output_processor=TakeFirst())
+    # tipo = Field(output_processor=TakeFirst())
 
 class CompraItem(Item):
     orden_compra = Field(output_processor=TakeFirst())
-    fecha = Field(output_processor=lambda x: spanish_date(*(x[0].split('/'))))
+    fecha = Field(output_processor=lambda x: spanish_date(*(string.split(x[0], '/'))))
     importe = Field(output_processor=lambda x: parse_money(x[0]))
-    proveedor = Field(output_processor=TakeFirst())
     suministro = Field(output_processor=TakeFirst())
+    proveedor = Field(output_processor=TakeFirst())
     destino = Field(output_processor=TakeFirst())
     # LPRI: Lic.Privada
     # LPUB: Lic. Publica
@@ -37,9 +38,8 @@ class CompraItem(Item):
     # CDVP: Compra directa varios proveedores
     # CONC: Concurso de precios
     tipo_compra = Field(output_processor=TakeFirst())
-    anio = Field(output_processor=TakeFirst())
-    compra_linea_items = Field()
-    tipo = Field(output_processor=TakeFirst())
+    
+    observaciones = Field(output_processor=TakeFirst())
 
 class CompraLineaItem(Item):
     cantidad = Field(output_processor=TakeFirst())
@@ -47,6 +47,7 @@ class CompraLineaItem(Item):
     importe = Field(output_processor=lambda x: parse_money(x[0]))
     importe_total = Field(output_processor=lambda x: parse_money(x[0]))
     detalle = Field(output_processor=TakeFirst())
+    orden_compra = Field(output_processor=TakeFirst())
     anio = Field(output_processor=TakeFirst())
 
 
